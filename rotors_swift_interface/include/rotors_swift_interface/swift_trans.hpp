fa42a94c8da76bca5c1e46ef3de494fd4e49e8d4
Arun P Madhu <stormbreaker.004@gmail.com>
2024-09-03 22:24:14 +0530

    feat: changed joy_interface -> swift_interface in ROS 2
    Replaced joy_interface with swift_interface in the ROS 2 project. The swift_interface now subscribes to a custom SwiftMsgs.msg, converts it into RollPitchYawrateThrust.msg, and publishes it. This message is then subscribed by the rotors_control package, enabling the new control interface.

--- rotors_swift_interface/include/rotors_swift_interface/swift_trans.hpp
+++ rotors_swift_interface/include/rotors_swift_interface/swift_trans.hpp
@@ -1,89 +1,58 @@
-/*
- * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
-
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#ifndef ROTORS_swift_INTERFACE_swift_HPP_
+#define ROTORS_swift_INTERFACE_swift_HPP_
 
 
-#ifndef ROTORS_JOY_INTERFACE_JOY_H_
-#define ROTORS_JOY_INTERFACE_JOY_H_
-
-#include <geometry_msgs/PoseStamped.h>
-#include <mav_msgs/RollPitchYawrateThrust.h>
-#include <ros/ros.h>
-#include <sensor_msgs/Joy.h>
+#include <memory>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <swift_msgs/msg/swift_msgs.hpp>
+#include <mav_msgs/msg/roll_pitch_yawrate_thrust.hpp>
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/logging.hpp"
 
 struct Axes {
-  int roll;
-  int pitch;
-  int thrust;
-  int roll_direction;
-  int pitch_direction;
-  int thrust_direction;
-};
-
-struct Buttons {
-  int takeoff;
-  int land;
-  int ctrl_enable;
-  int ctrl_mode;
-  int yaw_left;
-  int yaw_right;
+    int roll;
+    int pitch;
+    int thrust;
+    int roll_direction;
+    int pitch_direction;
+    int thrust_direction;
 };
 
 struct Max {
-  double v_xy;
-  double roll;
-  double pitch;
-  double rate_yaw;
-  double thrust;
+    double v_xy;
+    double roll;
+    double pitch;
+    double rate_yaw;
+    double thrust;
 };
 
-class Joy {
-  typedef sensor_msgs::Joy::_buttons_type ButtonType;
+class SwiftTrans : public rclcpp::Node
+{
 
- private:
-  ros::NodeHandle nh_;
-  ros::Publisher ctrl_pub_;
-  ros::Subscriber joy_sub_;
+    private:
+     rclcpp::Publisher<mav_msgs::msg::RollPitchYawrateThrust>::SharedPtr ctrl_pub;
+     rclcpp::Subscription<swift_msgs::msg::SwiftMsgs>::ConstSharedPtr swift_sub;
 
-  std::string namespace_;
+     //check whether to add namespace or not
 
-  Axes axes_;
-  Buttons buttons_;
+     Axes axes;
+     mav_msgs::msg::RollPitchYawrateThrust control_msg;
+     geometry_msgs::msg::PoseStamped pose;
+     Max max;
 
-  mav_msgs::RollPitchYawrateThrust control_msg_;
-  geometry_msgs::PoseStamped pose_;
-  sensor_msgs::Joy current_joy_;
+     double current_yaw_vel;
+     double div_factor;
+     double v_yaw_step;
+     double thrust_weight_offset_newtons;
 
-  Max max_;
+     void StopMav();
 
-  double current_yaw_vel_;
-  double v_yaw_step_;
+     void TransCallback(const swift_msgs::msg::SwiftMsgs::ConstSharedPtr &msg);
 
-  bool is_fixed_wing_;
+    public:
+     SwiftTrans();
+     void Publish();
 
-  void StopMav();
-
-  void JoyCallback(const sensor_msgs::JoyConstPtr& msg);
-  void Publish();
-
- public:
-  Joy();
 };
 
-#endif // ROTORS_JOY_INTERFACE_JOY_H_
+#endif
